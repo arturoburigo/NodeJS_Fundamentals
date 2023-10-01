aeaa0a29642298cd15c1383d98e3b390146b8208
Arturo Burigo <arturoburigo@Arturos-MacBook-Pro.local>
2023-09-30 23:46:14 -0300

    done2

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host:'RENDER' in process.env ? `0.0.0.0` : `localhost` }).then(() => {
   console.log('HTTP Server Running')
 })
